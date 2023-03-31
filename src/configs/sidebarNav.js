d2b5507d19379149560c822b382e822074ce7f97
phandung2701 <ptd012001@gmail.com>
2023-03-31 16:08:34 +0700

    first commit

---
+++ src/configs/sidebarNav.js
@@ -0,0 +1,52 @@
+const sidebarNav = [
+  {
+    link: 'dashboard',
+    section: 'home',
+    icon: <i className="bx bx-home-alt"></i>,
+    text: 'Home',
+  },
+  {
+    link: 'add-movie',
+    section: 'addfilm',
+    icon: <i className="bx bx-receipt"></i>,
+    text: 'Create New Movie',
+  },
+  {
+    link: 'movie',
+    section: 'Movie',
+    icon: <i className="bx bx-cube"></i>,
+    text: 'Movie',
+  },
+  {
+    link: 'account',
+    section: 'accounts',
+    icon: <i className="bx bx-user"></i>,
+    text: 'Account',
+  },
+  {
+    link: 'cinema',
+    section: 'Cinema',
+    icon: <i className="bx bx-camera-movie"></i>,
+    text: 'Cinema',
+  },
+  {
+    link: 'screen',
+    section: 'Screen',
+    icon: <i className="bx bx-chalkboard"></i>,
+    text: 'Screen',
+  },
+  {
+    link: 'showTime',
+    section: 'ShowTime',
+    icon: <i className="bx bx-time"></i>,
+    text: 'ShowTime',
+  },
+  {
+    link: '/',
+    section: 'settings',
+    icon: <i className="bx bx-cog"></i>,
+    text: 'Settings',
+  },
+];
+
+export default sidebarNav;
